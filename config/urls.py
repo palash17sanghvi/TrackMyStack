6d19acc4c8466b2912c8e780859b05ff06bc88df
Palash Sanghvi <palashsanghvi@palashs-air.3500.dhcp.asu.edu>
2026-06-04 12:36:38 -0700

    UI changes to make it pretty

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from two_factor.urls import urlpatterns as tf_urls
-
+from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('subscriptions.urls')),
     path('', include(tf_urls)),
+    path('', RedirectView.as_view(
+        pattern_name='dashboard', permanent=False), name='index'),
 ]
